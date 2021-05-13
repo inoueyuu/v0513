e77187ba7421264a3c788ed2bc2d31e5774e041a
Yuki <>
2021-05-13 11:16:34 +0900

    エラーの確認

--- v0513/Form1.cs
+++ v0513/Form1.cs
@@ -16,5 +16,16 @@ namespace v0513
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 1;
+            MessageBox.Show("" + 10 / a);
+        }
     }
 }
